15cedb41b91e7e59d0cb6b9e1ddfdf3b9d8a8972
Juan Carlos <jcrogel@gmail.com>
2018-11-21 21:02:50 -0800

    WIP

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,17 @@
 package sample;
 
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -17,6 +21,7 @@ public class Controller {
 
         @FXML // fx:id="list_view";
         ListView list_view;
+        Application current_app;
 
         public void pressed(Event evt) {
             CompletableFuture<String> cf = new CompletableFuture<String>();
@@ -33,6 +38,13 @@ public class Controller {
             String new_value = value_label.getText() + evt.getText();
             value_label.setText(new_value);
 
+        }
+
+        public void clicked(MouseEvent evt){
+            String item = list_view.getSelectionModel().getSelectedItem().toString();
+            String selected_item = item.split("\\(")[1].split("\\)")[0];
+            URL loc = list_view.getClass().getResource("popup.fxml");
+            PopupController.openWindow(selected_item);
         }
 
 }
--- src/sample/PopupController.java
+++ src/sample/PopupController.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class PopupController {
+    public static void openWindow(String selected_code){
+
+        Parent root = null;
+        try {
+            Stage window = new Stage();
+            URL loc = new URL("file:/Users/moreno/dev/OpenAir/out/production/OpenAir/sample/popup.fxml");
+
+            root = FXMLLoader.load(loc);
+            window.setTitle("Open Air - " + selected_code);
+            window.setScene(new Scene(root, 600, 400));
+            window.show();
+            Measurements.readFromAPI(selected_code);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
